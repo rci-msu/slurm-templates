9117404fce404ca1d89fe93f2a5a306c22ee401f
Trevor Vannoy <trevorvannoy@montana.edu>
2024-03-05 14:43:39 -0700

    use rsync to copy files to tempest-web
    rsync lets us set permissions for directories and files separately,
    which is not possible to do in one line of chmod.

--- deploy-to-ood.sh
+++ deploy-to-ood.sh
@@ -10,10 +10,16 @@ readonly REPO_JOB_TEMPLATE_DIR="ood"
 cd "${REPO_JOB_TEMPLATE_DIR}"
 
 # Copy all the template directories to tempest-web
+# We explicitly set the permissions because these files may be rsync'd from a
+# Windows machine, in which case the permissions will be incorrect.
 echo "copying templates..."
-scp -q -r * "root@${TEMPEST_WEB_HOSTNAME}:${TEMPEST_WEB_JOB_TEMPLATE_DIR}/"
 
-# We explicitly set the permissions because these files may be scp'd from a
-# Windows machine, in which case the permissions might be incorrect.
-echo "setting permissions..."
-ssh "root@${TEMPEST_WEB_HOSTNAME}" chmod -R u=rwX,g=rwX,o=rX "${TEMPEST_WEB_JOB_TEMPLATE_DIR}"
+rsync \
+  --recursive \
+  --perms \
+  --chmod=D775,F644 \
+  --links \
+  --copy-unsafe-links \
+  --exclude="README.md" \
+  ./* \
+  "root@${TEMPEST_WEB_HOSTNAME}:${TEMPEST_WEB_JOB_TEMPLATE_DIR}/"
